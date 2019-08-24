9bae72b93ba18dce8d85065f4c7f4b18ccb90c26
Seongwan Kim <seongwan.kim@outlook.kr>
2019-08-24 23:52:38 +0900

    Add dataReader Class

--- PyVideo/PyVideo.py
+++ PyVideo/PyVideo.py
@@ -2,13 +2,17 @@ import numpy as np
 import struct 
 import matplotlib.pyplot as plt
 
+
+class DataReader:
+    self.szBuffer = 16384
+    def __init__ (self, filename):
+        self.fp = open(filename, 'rb')
+        self.buf = self.fp.read(Datareader.szBuffer)
+
+
 class VideoContainer:
     def __init__(self):
         pass
-
-    def __init__(self, filename):
-        self.__init__()
-        self.open(filename)
 
     def open(self, filename):
         self.filename = filename
@@ -17,7 +21,13 @@ class VideoContainer:
         pass
 
     def open_AVI(self):
+        self.dr = DataReader(self.filename)
         pass
 
 if __name__ == '__main__':
     print("Main function")
+    # test avi
+    vc = VideoContainer()
+    vc.open('test.avi')
+
+    # test mkv
