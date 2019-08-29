71e3314169d59af12d5845f637111bbd76db2209
Seongwan Kim <seongwan.kim@outlook.kr>
2019-08-29 23:31:40 +0900

    Update structure add parse header

--- PyVideo/PyVideo.py
+++ PyVideo/PyVideo.py
@@ -19,7 +19,9 @@ class DataReader:
     
     def Int32(self):
         ret = self.GetBytes(4)
-        return int.from_bytes(ret, 'little')
+        sz = int.from_bytes(ret, 'little')
+        print('int32: ', sz)
+        return sz
 
 
 
@@ -33,48 +35,70 @@ class VideoContainer:
             self.open_AVI()
         pass
 
-    def Parse_unit(self, name = ''):
+    def parse_unit(self, name = ''):
+        list_names = ['LIST', 'RIFF', 'hdrl']
         if name == '':
             name = self.dr.GetBytes(4)
         if type(name) == bytes:
             name = name.decode('utf-8')
+        sz = self.dr.Int32()
+        tsz = sz
+        if name in list_names:
+            #parse list
+            print('fourcc', end='')
+            fourcc = self.dr.GetBytes(4)
+            pass
+        while tsz > 0:
+            psd = self.parse_fourcc(name)
+            tsz -= psd
+            print(f'parsed {name}, parsed: {psd}, remain unit: {tsz}, all: {sz}')
+        return sz
+
+    def parse_RIFF(self):
+        return self.parse_unit()
+
+    def parse_LIST(self):
+        return self.parse_unit()
+
+    def parse_AVI(self):
+        return self.parse_unit()
+
+    def parse_hdrl(self):
+        return self.parse_unit()
+
+    def parse_avih(self): # avi header 
+        self.msec_per_frame = self.dr.Int32()
+        self.Bytes_per_sec = self.dr.Int32()
+        self.padd_gran = self.dr.Int32()
+        self.flags = self.dr.Int32()
+        self.num_frames = self.dr.Int32()
+        self.init_frames = self.dr.Int32()
+        self.streams = self.dr.Int32()
+        self.szBuffer = self.dr.Int32()
+        self.width = self.dr.Int32()
+        self.height = self.dr.Int32()
+        self.reserved0 = self.dr.GetBytes(4)
+        self.reserved1 = self.dr.GetBytes(4)
+        self.reserved2 = self.dr.GetBytes(4)
+        self.reserved3 = self.dr.GetBytes(4)
+        return 56
+
+    def parse_fourcc(self, cc = ''):
+        if len(cc) == 0:
+            cc = self.dr.GetBytes(4)
+        if type(cc) == bytes:
+            cc = cc.decode('utf-8')
         try:
-            eval('self.Parse_' + name + '()')
+            sz = eval('self.parse_' + cc + '()')
         except Exception as e:
-            print(f'Exception occurred with parse_{name} : {e}')
+            print(f'error is occurred in fourcc parsing: {e}')
+        return sz
 
     def open_AVI(self):
         self.dr = DataReader(self.filename)
-        self.Parse_unit()
+        self.parse_unit()
         pass
 
-    def Parse_RIFF(self):
-        print('parse RIFF unit - size: ', self.dr.Int32())
-        self.Parse_unit()
-        pass
-
-    def Parse_AVI(self):
-        self.Parse_unit()
-        pass
-
-    def Parse_LIST(self):
-        sz = self.dr.Int32()
-        print(sz)
-        self.Parse_unit()
-        pass
-
-    def Parse_hdrl(self):
-        self.Parse_unit()
-        pass
-
-    def Parse_avih(self):
-        parm1 = self.dr.Int32()
-        parm2 = self.dr.Int32()
-        parm3 = self.dr.Int32()
-        parm4 = self.dr.Int32()
-
-        print('---')
-        pass
 
 if __name__ == '__main__':
     print("Main function")
